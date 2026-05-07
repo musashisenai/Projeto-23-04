1b467cdc635bbc80a9cf8af00a57d6b99162bd05
musashisenai <musashisenai@gmail.com>
2026-05-07 10:55:02 -0300

    update 07-05

--- app/Livewire/Movimentacao/MovimentacaoCreate.php
+++ app/Livewire/Movimentacao/MovimentacaoCreate.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire\Movimentacao;
 
+use App\Models\Movimentacao;
 use App\Models\Produto;
 use Livewire\Component;
 
@@ -21,5 +22,49 @@ class MovimentacaoCreate extends Component
     public function render()
     {
         return view('livewire.movimentacao.movimentacao-create');
+    }
+
+    public function store(){
+        $produto = Produto::find($this->idProdutoSelecionado);
+
+        if($produto->qtd_estoque < $this->quantidade_movimentada && $this->tipo == 'saida'){
+            $this->addError('quantidade_movimentada', 'Quantidade em estoque insuficiente');
+            return;
+        }
+
+        // Atualizar estoque 
+        if($this->tipo == 'entrada'){
+            //$produto->qtd_estoque = $produto->qtd_estoque + $this->quantidade_movimentada;
+            $produto->qtd_estoque += $this->quantidade_movimentada;
+            //$produto->increment('qtd_estoque', $this->quantidade_movimentada);
+        } else{
+            //$produto->qtd_estoque = $produto->qtd_estoque - $this->quantidade_movimentada;
+            $produto->qtd_estoque -= $this->quantidade_movimentada;
+            //$produto->decrement('qtd_estoque', $this->quantidade_movimentada);
+        }
+        
+        // Registrar Movimentação 
+        Movimentacao::create([
+            'quantidade'=> $this->quantidade_movimentada,
+            'data_movimentacao'=> $this->data_movimentacao,
+            'tipo' => $this->tipo,
+            'produto_id'=> $this-> idProdutoSelecionado,
+            'user_id'=> 1
+        ]);
+
+        $produto->update();
+
+        // Verificar estoque baixo
+
+        $produto->refresh();
+        if($produto->qtd_estoque < $produto->qtd_minima){
+            $this->alertaEstoqueBaixo = "ALERTA: Estoque baixo para {$produto->nome}. Quantidade Atual: {$produto->qtd_estoque}";
+        } else{
+            $this->alertaEstoqueBaixo ="";
+        }
+
+        session()->flash('message', 'Movimentação registrada com sucesso!');
+        $this->reset(['quantidade_movimentada', 'tipo']);
+        $this->produtos = Produto::orderby('nome')->get();
     }
 }
--- resources/views/livewire/movimentacao/movimentacao-index.blade.php
+++ resources/views/livewire/movimentacao/movimentacao-index.blade.php
@@ -1,3 +1,30 @@
-<div>
-    {{-- Care about people's approval and you will be their prisoner. --}}
+<div class="container">
+    <div class="card">
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Produto ID</th>
+                            <th>User ID</th>
+                            <th>Tipo</th>
+                            <th>Quantidade</th>
+                            <th>Data_Movimentação</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($movimentacaos as $m)
+                            <tr>
+                                <td>{($m->produto_id)}</td>
+                                <td>{($m->user_id)}</td>
+                                <td>{($m->tipo)}</td>
+                                <td>{($m->quantidade)}</td>
+                                <td>{($m->data_movimentacao)}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
