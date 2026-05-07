539253e8efbfab3f170918437dce88270322275b
musashisenai <musashisenai@gmail.com>
2026-05-07 11:03:08 -0300

    fix movimentacao-index.blade.php

--- resources/views/livewire/movimentacao/movimentacao-index.blade.php
+++ resources/views/livewire/movimentacao/movimentacao-index.blade.php
@@ -2,28 +2,28 @@
     <div class="card">
         <div class="card-body">
             <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>Produto ID</th>
-                            <th>User ID</th>
-                            <th>Tipo</th>
-                            <th>Quantidade</th>
-                            <th>Data_Movimentação</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($movimentacaos as $m)
+                                    <table class="table table-striped">
+                        <thead>
                             <tr>
-                                <td>{($m->produto_id)}</td>
-                                <td>{($m->user_id)}</td>
-                                <td>{($m->tipo)}</td>
-                                <td>{($m->quantidade)}</td>
-                                <td>{($m->data_movimentacao)}</td>
+                                <th>Produto ID</th>
+                                <th>User ID</th>
+                                <th>Tipo</th>
+                                <th>Quantidade</th>
+                                <th>Data_Movimentação</th>
                             </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            @foreach ($movimentacaos as $m)
+                                <tr>
+                                    <td>{{ $m->produto_id }}</td>
+                                    <td>{{ $m->user_id }}</td>
+                                    <td>{{$m->tipo}}</td>
+                                    <td>{{ $m->quantidade }}</td>
+                                    <td>{{ $m->data_movimentacao }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
             </div>
         </div>
     </div>
